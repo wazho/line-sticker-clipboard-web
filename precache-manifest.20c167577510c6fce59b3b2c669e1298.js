8fbf7ad444c87f59ca1419620ef9ecdaf01b7159
Ze-Hao Wang (a.k.a. Salmon) <salmon.zh.tw@gmail.com>
2019-12-05 23:12:57 +0800

    First github page.

---
+++ precache-manifest.20c167577510c6fce59b3b2c669e1298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b985c6d4067a0f4487f127a1efe39d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47af0e7f484b2cc3af31",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "9907825d1cec4a40b0b4",
+    "url": "/static/css/main.d15e702f.chunk.css"
+  },
+  {
+    "revision": "47af0e7f484b2cc3af31",
+    "url": "/static/js/2.b064d885.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/static/js/2.b064d885.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9907825d1cec4a40b0b4",
+    "url": "/static/js/main.58d3dd08.chunk.js"
+  },
+  {
+    "revision": "4a9a9ef60b0982e38be4",
+    "url": "/static/js/runtime-main.daab41d8.js"
+  }
+]);
