f9925f6e542cf53ef802343af799726c5d399f2d
githubaction@fake.com <githubaction>
2021-10-08 05:19:36 +0000

    deploy to Github pages

---
+++ precache-manifest.0983c245756c02b1d18299b5e9d7a234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37947816c2b92f7c7b54e2fb274568ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8209ff270312133f605",
+    "url": "/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "bc4457237711a2ff8bda",
+    "url": "/static/css/main.d15e702f.chunk.css"
+  },
+  {
+    "revision": "f8209ff270312133f605",
+    "url": "/static/js/2.ac67cf10.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/static/js/2.ac67cf10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc4457237711a2ff8bda",
+    "url": "/static/js/main.0b3c95dd.chunk.js"
+  },
+  {
+    "revision": "4a9a9ef60b0982e38be4",
+    "url": "/static/js/runtime-main.daab41d8.js"
+  }
+]);
